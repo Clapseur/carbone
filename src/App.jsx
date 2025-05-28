0ff380a64f9d1cf237207b0166b903fc7904a98e
Clapseur <gangnou27.amorin@gmail.com>
2025-05-28 11:52:54 +0200

    hot fix + tailwind adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
 import Aurora from "./AuroraBackground.jsx";
+import Header from "./Header.jsx";
 
 function App() {
   return (
     <>
+      <Header />
       <Aurora
         colorStops={["#f4f4f4", "#ffffff", "#333333"]}
         blend={0.5}
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <header>Carbone x </header>
+        <img src="assets/genesii-logo-black.svg" alt="" />;
+      </div>
+    </>
+  );
+}
